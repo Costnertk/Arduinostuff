0331b638ac08cdcf8ae7213d8fec3a89b5268e07
Costnertk <58087296+Costnertk@users.noreply.github.com>
2020-04-18 18:57:51 -0400

    push

--- ModelRocket/StartupStandby/StartupStandby.ino
+++ ModelRocket/StartupStandby/StartupStandby.ino
@@ -45,11 +45,11 @@ void startup() {
   ServoTest();
 }
 
-void standby() {
+void standby(){
 
 }
 
-void setupMPU() {
+void setupMPU(){
   Wire.beginTransmission(0b1101000); //This is the I2C address of the MPU (b1101000/b1101001 for AC0 low/high datasheet sec. 9.2)
   Wire.write(0x6B); //Accessing the register 6B - Power Management (Sec. 4.28)
   Wire.write(0b00000000); //Setting SLEEP register to 0. (Required; see Note on p. 9)
