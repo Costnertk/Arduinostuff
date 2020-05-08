d4f4f42db1e70e04220a20dfe89d815c6e3a2d3a
Costnertk <58087296+Costnertk@users.noreply.github.com>
2020-05-08 11:45:24 -0400

    bmp280 and mpu6050 test

--- bmp280andmpu6050/bmp280andmpu6050.ino
+++ bmp280andmpu6050/bmp280andmpu6050.ino
@@ -1,5 +1,10 @@
 #include <Wire.h>
 
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+
 long accelX, accelY, accelZ;
 float gForceX, gForceY, gForceZ;
 
@@ -7,20 +12,109 @@ long gyroX, gyroY, gyroZ;
 float rotX, rotY, rotZ;
 float accAngX, accAngY;
 
+Adafruit_BME280 bme; // I2C
+
 void setup() {
-  Serial.begin(9600);
-  Wire.begin();
-  setupMPU();
+    Serial.begin(9600);
+    Serial.println(F("BME280 and MPU6050 test"));
+
+    bool status;
+    
+    // default settings
+    // (you can also pass in a Wire library object like &Wire2)
+    status = bme.begin(0x76);  
+    if (!status) {
+        Serial.println("Could not find a valid BME280 sensor, check wiring!");
+        while (1);
+    }
+    
+    Wire.begin();
+    setupMPU();
+    
+    Serial.println("Ready");
+    delay(2000);
+
+    Serial.println();
 }
 
+void loop() { 
+    
+    Serial.println("--------------------");
+    Serial.println();
+    Serial.print("Temperature = ");
+    Serial.print(getBME('C'));
+    printDegree();
+    Serial.print("C ");
+    Serial.print(getBME('F'));
+    printDegree();
+    Serial.print("F "); 
+    Serial.print(getBME('K'));
+    Serial.println("K ");       
 
-void loop() {
-  recordAccelRegisters();
-  recordGyroRegisters();
-  calcualteRP();
-  printData();
-  delay(100);
-}
+
+    Serial.print("Pressure = ");
+    Serial.print(getBME('P'));
+    Serial.println(" hPa ");
+
+    Serial.print("Humidity = ");
+    Serial.print(getBME('H'));
+    Serial.println("% ");
+    
+    Serial.print("Approx. Altitude = ");
+    Serial.print(getBME('A'));
+    Serial.println(" m");
+
+
+
+    Serial.println();
+
+    // action
+    if(getBME('C') <65.2)
+    {
+     //digitalWrite(5, HIGH); 
+    }
+
+    recordAccelRegisters();
+    recordGyroRegisters();
+    calcualteRP();
+    printData();
+
+    delay(2000);
+}// loop end
+
+float getBME(char type)
+{
+  float value;
+    float temp = bme.readTemperature();// read temperature
+    float pressure = bme.readPressure() / 100.0F; // read pressure
+    float rel_hum = bme.readHumidity();// read humidity
+    float alt =bme.readAltitude(SEALEVELPRESSURE_HPA);// read altitude
+   if(type =='F')
+   {
+    value = temp *9/5 + 32;//convert to Fahrenheit 
+   }else if(type =='K')
+   {
+    value = temp + 273.15;//convert to Kelvin
+   }else if(type =='H')
+   {
+    value = rel_hum;//return relative humidity
+   }else if(type =='P')
+   {
+    value = pressure;//return pressure
+   }else if(type =='A')
+   {
+    value = alt;//return approximate altitude
+   }else{
+    value = temp;// return Celsius
+   }
+   return value;
+}//getBME
+
+void printDegree()
+{
+    Serial.print("\xC2"); 
+    Serial.print("\xB0");  
+} 
 
 void setupMPU(){
   Wire.beginTransmission(0b1101000); //This is the I2C address of the MPU (b1101000/b1101001 for AC0 low/high datasheet sec. 9.2)
@@ -86,17 +180,21 @@ void printData() {
   Serial.print(" Y=");
   Serial.print(rotY);
   Serial.print(" Z=");
-  Serial.print(rotZ);
-  Serial.println(" Accel (g)");
+  Serial.println(rotZ);
+  Serial.println("Accel (g)");
   Serial.print(" X=");
   Serial.print(gForceX);
   Serial.print(" Y=");
   Serial.print(gForceY);
   Serial.print(" Z=");
   Serial.println(gForceZ);
-  Serial.print(" Roll = ");
+  Serial.print("Roll = ");
   Serial.print(accAngX);
   Serial.print(" Pitch = ");
   Serial.println(accAngY);
+  Serial.println();
+  Serial.println("--------------------");
+  Serial.println();
+  Serial.println();
   
 }
